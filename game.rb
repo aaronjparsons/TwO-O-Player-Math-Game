aa6f72204c54693d8b22573407e0c46f02f03207
Aaron Parsons <aaron.parsons@live.ca>
2018-10-08 23:17:32 +0000

    finished game loop, broke some logic out into their own functions

--- game.rb
+++ game.rb
@@ -2,30 +2,59 @@ class Game
   def initialize(player1, player2, questions)
     @player1 = player1
     @player2 = player2
+    @current_player = player1
     @questions = questions
     @winner = false
   end
 
-  def players_still_have_lives
-    if @player1.lives > 0 || @player2.lives > 0
-      true
+  def switch_player
+    if @current_player == @player1
+      @current_player = @player2
     else
-      false
+      @current_player = @player1
     end
+  end
+
+  def players_still_have_lives?
+    @player1.lives <= 0 || @player2.lives <= 0
+  end
+
+  def handle_question
+    current_question = @questions.create_question
+    puts "#{@current_player.name}: #{current_question[:question]} (#{current_question[:answer]})"
+    answer = yield
+    if answer != current_question[:answer]
+      puts "Sorry, that's incorrect!"
+      @current_player.lose_a_life
+    else
+      puts "Correct! Nicely done."
+    end
+    handle_end_of_round
+  end
+
+  def handle_end_of_round
+    if (!players_still_have_lives?)
+      switch_player
+      puts "Player 1 lives: #{@player1.lives}/#{@player1.max_lives} vs Player 2 lives: #{@player2.lives}/#{@player2.max_lives}"
+      puts "------ NEW TURN ------"
+    else
+      @winner = true
+    end
+  end
+
+  def game_over
+    winner = @player1.lives != 0 ? @player1 : @player2
+    puts "#{winner.name} wins the game with a score of #{winner.lives}/#{winner.max_lives}!"
+    puts "------ GAME OVER ------"
+    puts "Good bye!"
   end
 
   # Game Loop
   def play_game
     while !@winner do
-      if players_still_have_lives
-        current_question = @questions.create_question
-        puts "#{current_question[:question]} (#{current_question[:answer]})"
-        answer = gets.chomp.to_i
-        if answer == current_question[:answer]
-          @winner = true
-        end
-      end
+      handle_question { gets.chomp.to_i }
     end
+    game_over
   end
 
 end
--- player.rb
+++ player.rb
@@ -1,12 +1,15 @@
 class Player
 
-  attr_accessor :lives
-  attr_reader :name, :max_lives
+  attr_reader :name, :lives, :max_lives
 
   def initialize(name)
     @name = name
     @max_lives = 3
     @lives = @max_lives
+  end
+
+  def lose_a_life
+    @lives -= 1
   end
 
 end
